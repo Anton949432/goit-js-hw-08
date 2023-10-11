e9b7589abdeb8784f68c133566d3e98f767f79e8
Anton949432 <antonbratcikov@gmail.com>
2023-10-11 21:56:50 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,13 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
+const saveCurrentTime = throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
 player.on('timeupdate', function (data) {
     const currentTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime);
+    saveCurrentTime(currentTime);
 });
 
 const savedTime = parseFloat(localStorage.getItem('videoplayer-current-time'));
